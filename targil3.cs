4fbccd2272e130627f7e39f9401d181d83db0f9a
bensimonlidor <47791397+bensimonlidor@users.noreply.github.com>
2019-03-14 19:10:45 +0200

    Update targil3.cs

--- targil3.cs
+++ targil3.cs
@@ -21,3 +21,37 @@ namespace targil2
         }
     }
 }
+
+
+
+
+
+op2
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace targil2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a=7; a>=1; a--)
+            {
+                int i;
+                for (i = 1; i < a; i++)
+                {
+                    Console.Write(i);
+                }
+                for (int j = i; j < 7; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
